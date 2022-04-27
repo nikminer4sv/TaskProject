f3350f1e53e4ec569ba453b3e15f657794402419
Nikita <nikminer4sv@gmail.com>
2022-04-27 14:44:30 +0300

    Update ApiController.cs

--- Server/Server/Controllers/ApiController.cs
+++ Server/Server/Controllers/ApiController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using System.IO;
@@ -12,193 +12,179 @@ using System.Linq;
 
 namespace Server.Controllers
 {
-	[ApiController]
-	[Route("[controller]")]
-	public class ApiController : ControllerBase
-	{
+    [ApiController]
+    [Route ("[controller]")]
+    public class ApiController : ControllerBase
+    {
 
-		private string IMAGES_FOLDER_PATH;
-		private string CARDS_FILE_PATH;
-		private string CARDS_FILE_NAME;
+        private string IMAGES_FOLDER_PATH;
+        private string CARDS_FILE_PATH;
+        private string CARDS_FILE_NAME;
 
-		public ApiController(IConfiguration config)
-		{
-			IMAGES_FOLDER_PATH = config["ImagesFolderPath"];
-			CARDS_FILE_PATH = config["Cards:CardsFilePath"];
-			CARDS_FILE_NAME = config["Cards:CardsFileName"];
+        public ApiController (IConfiguration config)
+        {
+            IMAGES_FOLDER_PATH = config["ImagesFolderPath"];
+            CARDS_FILE_PATH = config["Cards:CardsFilePath"];
+            CARDS_FILE_NAME = config["Cards:CardsFileName"];
 
-			if (!Directory.Exists(IMAGES_FOLDER_PATH))
-				Directory.CreateDirectory(IMAGES_FOLDER_PATH);
+            if (!Directory.Exists (IMAGES_FOLDER_PATH))
+                Directory.CreateDirectory (IMAGES_FOLDER_PATH);
 
-			if (!Directory.Exists(CARDS_FILE_PATH))
-				Directory.CreateDirectory(CARDS_FILE_PATH);
+            if (!Directory.Exists (CARDS_FILE_PATH))
+                Directory.CreateDirectory (CARDS_FILE_PATH);
 
-			if (!System.IO.File.Exists(CARDS_FILE_PATH + CARDS_FILE_NAME))
-				using (FileStream fs = System.IO.File.Create(CARDS_FILE_PATH + CARDS_FILE_NAME));
-		}
-
-		[HttpPost("card")]
-		public void Add(string title, IFormFile image)
-		{
-
-			if (title.Length > 19)
-				title = title.Substring(0, 19);
-
-			// WORK WITH IMAGE
-			string fileName = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString() + ".jpg";
-
-			using (var ms = new MemoryStream())
-			{
-				image.CopyTo(ms);
-				var fileBytes = ms.ToArray();
-				System.IO.File.WriteAllBytes(IMAGES_FOLDER_PATH + fileName, fileBytes);
-			}
-
-			// CREATING NEW CARD
-			Card card = new Card()
-			{
-				Id = Guid.NewGuid().ToString(),
-				Title = title,
-				ImageUri = fileName
-			};
-
-			// GETTING CARDS LIST FROM THE FILE
-			StreamReader reader = new StreamReader(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			string cardsStr = reader.ReadToEnd();
-			reader.Close();
-			
-			List<Card> cards;
-			if (cardsStr == "")
-				cards = new List<Card>();
-			else 
-				cards = JsonSerializer.Deserialize<List<Card>>(cardsStr);
-
-			// ADDING NEW CARD
-			cards.Add(card);
-
-			// WRITING UPDATED LIST TO THE FILE
-			using StreamWriter writer = new StreamWriter(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			writer.Write(JsonSerializer.Serialize<List<Card>>(cards));
-
+            if (!System.IO.File.Exists (CARDS_FILE_PATH + CARDS_FILE_NAME))
+                using (FileStream fs = System.IO.File.Create (CARDS_FILE_PATH + CARDS_FILE_NAME));
         }
 
-		[HttpGet("card/{Id}/image")]
-		public ActionResult GetCardImage(string Id)
+        [HttpPost ("card")]
+        public void Add (string title, IFormFile image)
         {
-			using StreamReader reader = new StreamReader(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			string cardsStr = reader.ReadToEnd();
-			List<Card> cards = JsonSerializer.Deserialize<List<Card>>(cardsStr);
-			Card card = cards.FirstOrDefault(c => c.Id == Id);
-			if (card != null)
+
+            if (title.Length > 19)
+                title = title.Substring (0, 19);
+
+            // WORK WITH IMAGE
+            string fileName = DateTimeOffset.Now.ToUnixTimeMilliseconds ().ToString () + ".jpg";
+
+            using (var ms = new MemoryStream ())
             {
-				return File(System.IO.File.ReadAllBytes(IMAGES_FOLDER_PATH + card.ImageUri), "image/*", card.ImageUri);
-            } else
-            {
-				return Content("");
+                image.CopyTo (ms);
+                var fileBytes = ms.ToArray ();
+                System.IO.File.WriteAllBytes (IMAGES_FOLDER_PATH + fileName, fileBytes);
             }
 
-		}
+            // CREATING NEW CARD
+            Card card = new Card (){ Id = Guid.NewGuid ().ToString (), Title = title, ImageUri = fileName };
 
-		[HttpGet("card/{Id}")]
-		public string GetCard(string Id)
+            // GETTING CARDS LIST FROM THE FILE
+            StreamReader reader = new StreamReader (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            string cardsStr = reader.ReadToEnd ();
+            reader.Close ();
+
+            List<Card> cards;
+            if (cardsStr == "")
+                cards = new List<Card> ();
+            else
+                cards = JsonSerializer.Deserialize<List<Card> > (cardsStr);
+
+            // ADDING NEW CARD
+            cards.Add (card);
+
+            // WRITING UPDATED LIST TO THE FILE
+            using StreamWriter writer = new StreamWriter (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            writer.Write (JsonSerializer.Serialize<List<Card> > (cards));
+        }
+
+        [HttpGet ("card/{Id}/image")]
+        public ActionResult GetCardImage (string Id)
         {
-			using StreamReader reader = new StreamReader(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			string cardsStr = reader.ReadToEnd();
-			List<Card> cards = JsonSerializer.Deserialize<List<Card>>(cardsStr);
-			Card card = cards.FirstOrDefault(c => c.Id == Id);
-			if (card != null)
-				return JsonSerializer.Serialize<Card>(card);
-			else
-				return "";
-		}
+            using StreamReader reader = new StreamReader (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            string cardsStr = reader.ReadToEnd ();
+            List<Card> cards = JsonSerializer.Deserialize<List<Card> > (cardsStr);
+            Card card = cards.FirstOrDefault (c => c.Id == Id);
 
-		[HttpGet("card")]
-		public string GetAllCards()
+            if (card != null)
+                return File (System.IO.File.ReadAllBytes (IMAGES_FOLDER_PATH + card.ImageUri), "image/*",
+                             card.ImageUri);
+            else
+                return Content ("");
+        }
+
+        [HttpGet ("card/{Id}")]
+        public string GetCard (string Id)
         {
-			using StreamReader reader = new StreamReader(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			return reader.ReadToEnd();
-		}
+            using StreamReader reader = new StreamReader (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            string cardsStr = reader.ReadToEnd ();
+            List<Card> cards = JsonSerializer.Deserialize<List<Card> > (cardsStr);
+            Card card = cards.FirstOrDefault (c => c.Id == Id);
 
-		public static byte[] CopyImageToByteArray(Image image)
-		{
-			using (MemoryStream memoryStream = new MemoryStream())
-			{
-				image.SaveAsJpeg(memoryStream);
-				return memoryStream.ToArray();
-			}
-		}
+            if (card != null)
+                return JsonSerializer.Serialize<Card> (card);
+            else
+                return "";
+        }
 
-		public static Image GetImageFromByteArray(byte[] byteArray) => Image.Load<Rgba32>(byteArray);
-		
-		[HttpDelete("card/delete")]
-		public void DeleteCards([FromQuery]string[] ids) 
+        [HttpGet ("card")]
+        public string GetAllCards ()
         {
+            using StreamReader reader = new StreamReader (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            return reader.ReadToEnd ();
+        }
 
-			string cardsStr = GetAllCards();
-			List<Card> cards = JsonSerializer.Deserialize<List<Card>>(cardsStr);
-			for (int i = 0; i < ids.Length; i++)
+        public static byte[] CopyImageToByteArray (Image image)
+        {
+            using (MemoryStream memoryStream = new MemoryStream ())
             {
-				for (int j = 0; j < cards.Count; j++)
+                image.SaveAsJpeg (memoryStream);
+                return memoryStream.ToArray ();
+            }
+        }
+
+        public static Image GetImageFromByteArray (byte[] byteArray) => Image.Load<Rgba32> (byteArray);
+
+        [HttpDelete ("card/delete")]
+        public void DeleteCards ([ FromQuery ] string[] ids)
+        {
+
+            string cardsStr = GetAllCards ();
+            List<Card> cards = JsonSerializer.Deserialize<List<Card> > (cardsStr);
+            for (int i = 0; i < ids.Length; i++)
                 {
-					if (ids[i] == cards[j].Id)
-                    {
-						System.IO.File.Delete(IMAGES_FOLDER_PATH + cards[j].ImageUri);
-						cards.RemoveAt(j);
-						break;
-                    }
+                    for (int j = 0; j < cards.Count; j++)
+                        {
+                            if (ids[i] == cards[j].Id)
+                                {
+                                    System.IO.File.Delete (IMAGES_FOLDER_PATH + cards[j].ImageUri);
+                                    cards.RemoveAt (j);
+                                    break;
+                                }
+                        }
                 }
-            }
-			using StreamWriter writer = new StreamWriter(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			writer.Write(JsonSerializer.Serialize<List<Card>>(cards));
+            using StreamWriter writer = new StreamWriter (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            writer.Write (JsonSerializer.Serialize<List<Card> > (cards));
+        }
 
-		}
-
-		[HttpPut("card/fullupdate")]
-		public void FullUpdate(string id, string title, IFormFile image)
+        [HttpPut ("card/fullupdate")]
+        public void FullUpdate (string id, string title, IFormFile image)
         {
+            if (title.Length > 19)
+                title = title.Substring (0, 19);
 
-			if (title.Length > 19)
-				title = title.Substring(0, 19);
+            string cardsStr = GetAllCards ();
+            List<Card> cards = JsonSerializer.Deserialize<List<Card> > (cardsStr);
+            string fileName = DateTimeOffset.Now.ToUnixTimeMilliseconds ().ToString () + ".jpg";
 
-			string cardsStr = GetAllCards();
-			List<Card> cards = JsonSerializer.Deserialize<List<Card>>(cardsStr);
-			string fileName = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString() + ".jpg";
+            using (var ms = new MemoryStream ())
+            {
+                image.CopyTo (ms);
+                var fileBytes = ms.ToArray ();
+                System.IO.File.WriteAllBytes (IMAGES_FOLDER_PATH + fileName, fileBytes);
+            }
 
-			using (var ms = new MemoryStream())
-			{
-				image.CopyTo(ms);
-				var fileBytes = ms.ToArray();
-				System.IO.File.WriteAllBytes(IMAGES_FOLDER_PATH + fileName, fileBytes);
-			}
+            Card card = cards.FirstOrDefault (c => c.Id == id);
+            System.IO.File.Delete (IMAGES_FOLDER_PATH + card.ImageUri);
+            card.Title = title;
+            card.ImageUri = fileName;
 
-			Card card = cards.FirstOrDefault(c => c.Id == id);
-			System.IO.File.Delete(IMAGES_FOLDER_PATH + card.ImageUri);
-			card.Title = title;
-			card.ImageUri = fileName;
+            using StreamWriter writer = new StreamWriter (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            writer.Write (JsonSerializer.Serialize<List<Card> > (cards));
+        }
 
-			using StreamWriter writer = new StreamWriter(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			writer.Write(JsonSerializer.Serialize<List<Card>>(cards));
+        [HttpPut ("card/simpleupdate")]
+        public void SimpleUpdate (string id, string title)
+        {
+            if (title.Length > 19)
+                title = title.Substring (0, 19);
 
-		}
+            string cardsStr = GetAllCards ();
+            List<Card> cards = JsonSerializer.Deserialize<List<Card> > (cardsStr);
 
-		[HttpPut("card/simpleupdate")]
-		public void SimpleUpdate(string id, string title)
-		{
+            Card card = cards.FirstOrDefault (c => c.Id == id);
+            card.Title = title;
 
-			if (title.Length > 19)
-				title = title.Substring(0, 19);
-
-			string cardsStr = GetAllCards();
-			List<Card> cards = JsonSerializer.Deserialize<List<Card>>(cardsStr);
-
-			Card card = cards.FirstOrDefault(c => c.Id == id);
-			card.Title = title;
-
-			using StreamWriter writer = new StreamWriter(CARDS_FILE_PATH + CARDS_FILE_NAME);
-			writer.Write(JsonSerializer.Serialize<List<Card>>(cards));
-
-		}
-
-	}
+            using StreamWriter writer = new StreamWriter (CARDS_FILE_PATH + CARDS_FILE_NAME);
+            writer.Write (JsonSerializer.Serialize<List<Card> > (cards));
+        }
+    }
 }
-
